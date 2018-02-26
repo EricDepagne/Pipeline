8c5f042a8c5a6ee17ac933609f8a1dd6ec3994e2
EricDepagne <eric@depagne.org>
2018-02-26 11:41:09 +0200

    Update
    Signed-off-by: EricDepagne <eric@depagne.org>

--- __init__.py
+++ __init__.py
@@ -1,3 +1,32 @@
 """
 This modules processes the HRS data and produces a 1D spectrum
 """
+import six
+import distutils.version
+
+from . import stability
+from .window import MainWindow
+
+def compare_versions(a, b):
+    "return True if a is greater than or equal to b"
+    if a:
+        if six.PY3:
+            if isinstance(a, bytes):
+                a = a.decode('ascii')
+            if isinstance(b, bytes):
+                b = b.decode('ascii')
+        a = distutils.version.LooseVersion(a)
+        b = distutils.version.LooseVersion(b)
+        return a >= b
+    else:
+        return False
+
+try:
+    import astropy
+except ImportError:
+    raise ImportError("Matplotlib requires astropy")
+else:
+    if not compare_versions(astropy.__version__, '2.0.1'):
+        raise ImportError(
+            "Matplotlib requires astropy>=4.0.1; you have %s"
+            % astropy.__version__)
