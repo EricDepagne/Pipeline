3ee1a85353c11b699242721b7d364ab4c7b4eb28
EricDepagne <eric@depagne.org>
2018-04-13 19:45:43 +0200

    Do not normalise the orders by their width

--- stability/stability.py
+++ stability/stability.py
@@ -559,7 +559,7 @@ class Extract(object):
                 continue
             for i in x:
                 try:
-                    orders[o, i] = data[np.int(foinf(i)):np.int(foinf(i)) + orderwidth, i].sum()/orderwidth
+                    orders[o, i] = data[np.int(foinf(i)):np.int(foinf(i)) + orderwidth, i].sum()
                 except ValueError:
                     continue
 # TODO:
