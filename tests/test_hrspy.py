dfa081b3c9ac6d8753059edecc683a2ea366da8f
EricDepagne <eric@depagne.org>
2018-06-07 01:42:17 +0200

    useless

--- tests/test_hrspy.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from pipeline.hrspy import HRS
-from astropy.io import fits
-
-
-def test_FITS():
-    file = 'test/R201704090006.fits'
-    f = HRS(file)
