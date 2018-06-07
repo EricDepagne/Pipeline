d7c9eb9275bd399fdda6c44b01844c71446b0424
EricDepagne <eric@depagne.org>
2018-06-07 22:35:24 +0200

    ListOfFiles Tests

---
+++ tests/test_fits.py
@@ -0,0 +1,16 @@
+import pytest
+from pathlib import Path
+
+from pipeline.hrspy import ListOfFiles
+
+
+def test_listoffile():
+    directory = Path('./pipeline/tests/data')
+    lof = ListOfFiles(directory)
+    assert(len(lof.bias) == 1)
+    assert(len(lof.flat) == 1)
+    assert(len(lof.thar) == 1)
+    assert(len(lof.science) == 3)
+    assert(len(lof.specphot) == 1)
+    assert(len(lof.object) == 1)
+    assert(len(lof.sky) == 1)
