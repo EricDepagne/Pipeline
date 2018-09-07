2c2fef2e485804f92c8b8cfb09992aa81844619e
EricDepagne <eric@depagne.org>
2018-09-07 15:25:27 +0200

    Adding pyhrs tests

--- tests/test_reduced_files.py
+++ tests/test_reduced_files.py
@@ -1,6 +1,11 @@
 import pytest
 
 def test_pyhrs():
+    """
+    Check whether we extract the pyhrs content properly.
+    """
+    bluefile = 'pH201808280017_obj.fits'
+    redfile = 'pR201808280017_obj.fits'
     pass
 
 def test_midashrs():
